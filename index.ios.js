410503f6bd942cf232bfa666686aa114f9e6c26f
Max Petruck <petruck.maxim@gmail.com>
2016-01-15 15:10:28 +0200

    Add custom scene transition configuration

--- index.ios.js
+++ index.ios.js
@@ -10,6 +10,20 @@ import Navbar from "./components/Navbar.js";
 import Scene from "./components/Scene.js";
 import styles from "./styles/Stylesheet.js";
 
+const BaseConfig = Navigator.SceneConfigs.FloatFromBottom;
+const CustomGesture = Object.assign({}, BaseConfig.gestures.pop, {
+  snapVelocity: 6,
+});
+const CustomSceneConfig = Object.assign({}, BaseConfig, {
+  springTension: 100,
+  springFriction: 6,
+  gestures: {
+    pop: CustomGesture
+  }
+});
+
+console.log(BaseConfig);
+
 class App extends Component {
   _renderScene(route, navigator) {
     return (
@@ -29,12 +43,16 @@ class App extends Component {
       />
     );
   }
+  _configureScene() {
+    return CustomSceneConfig;
+  }
   render() {
     return (
       <Navigator
         initialRoute={{name: "Scene", index: 0}}
         navigationBar={<Navbar/>}
         renderScene={this._renderScene}
+        configureScene={this._configureScene}
       />
     );
   }
