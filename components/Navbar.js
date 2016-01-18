e5fc09b4cef95be6915d44979250e06b069de775
Max Petruck <petruck.maxim@gmail.com>
2016-01-18 13:57:08 +0200

    Add three buttons to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,27 @@
 'use strict';
-import React, { Component, Text } from 'react-native';
+import React, { Component, Text, View, TouchableOpacity } from 'react-native';
 import styles from "../styles/Stylesheet.js";
 
 class Navbar extends Component {
   render() {
     return (
-      <Text style={styles.title}>
-        Navbar component
-      </Text>
+      <View style={styles.navbarContainer}>
+        <TouchableOpacity onPress={this.props.onScene1}>
+          <Text style={styles.button}>
+            Scene1
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.props.onScene2}>
+          <Text style={styles.button}>
+            Scene2
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.props.onScene3}>
+          <Text style={styles.button}>
+            Scene3
+          </Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
