361decf101bdc9ab99a7d3ad1d5299fc04a61601
Max Petruck <petruck.maxim@gmail.com>
2016-01-19 18:00:46 +0200

    Move all logic to App component, index just registers App component

--- index.ios.js
+++ index.ios.js
@@ -1,44 +1,4 @@
-import React, {
-  AppRegistry,
-  Component,
-  Navigator
-} from 'react-native';
+import { AppRegistry, } from 'react-native';
+import App from './src/App.js';
 
-import Navbar from "./components/Navbar.js";
-import Scene from "./components/Scene";
-import styles from "./styles/Stylesheet.js";
-
-const BaseConfig = Navigator.SceneConfigs.FloatFromBottom;
-const CustomGesture = Object.assign({}, BaseConfig.gestures.pop, {
-  snapVelocity: 6,
-});
-const CustomSceneConfig = Object.assign({}, BaseConfig, {
-  springTension: 100,
-  springFriction: 6,
-  gestures: {
-    pop: CustomGesture
-  }
-});
-
-class App extends Component {
-  _renderScene(route, navigator) {
-    return (
-      <Scene name={route.name}/>
-    );
-  }
-  _configureScene() {
-    return CustomSceneConfig;
-  }
-  render() {
-    return (
-      <Navigator
-        initialRoute={{name: "Scene1", index: 1}}
-        navigationBar={<Navbar/>}
-        renderScene={this._renderScene}
-        configureScene={this._configureScene}
-      />
-    );
-  }
-}
-
-AppRegistry.registerComponent('RNPlayground', () => App);
+AppRegistry.registerComponent(`RNPlayground`, () => App);
