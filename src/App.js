76a1510f6797b9f3c21fa368868c79c85430a986
Max Petruck <petruck.maxim@gmail.com>
2016-01-19 17:55:28 +0200

    App component refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, {
-  AppRegistry,
-  Component,
-  Navigator
-} from 'react-native';
-
-import Navbar from "./components/Navbar.js";
-import Scene from "./components/Scene";
-import styles from "./styles/Stylesheet.js";
+import React, { Component, Navigator, } from 'react-native';
+import Navbar from './components/Navbar.js';
+import Scene from './components/Scene';
 
 const BaseConfig = Navigator.SceneConfigs.FloatFromBottom;
 const CustomGesture = Object.assign({}, BaseConfig.gestures.pop, {
@@ -16,25 +10,26 @@ const CustomSceneConfig = Object.assign({}, BaseConfig, {
   springTension: 100,
   springFriction: 6,
   gestures: {
-    pop: CustomGesture
-  }
+    pop: CustomGesture,
+  },
 });
 
 class App extends Component {
   _renderScene(route, navigator) {
     return (
-      <Scene name={route.name}
-          onForward={() => {
+      <Scene
+        name={route.name}
+        onBack={() => {
+          if (route.index > 0) {
+            navigator.pop();
+          }
+        }}
+        onForward={() => {
             let nextIndex = route.index + 1;
             navigator.push({
-              name: 'Scene' + nextIndex,
-              index: nextIndex
+              name: `Scene` + nextIndex,
+              index: nextIndex,
             });
-          }}
-          onBack={() => {
-            if (route.index > 0) {
-              navigator.pop();
-            }
           }}
       />
     );
@@ -45,11 +40,13 @@ class App extends Component {
   render() {
     return (
       <Navigator
-        initialRoute={{name: "Scene", index: 0}}
+        configureScene={this._configureScene}
+        initialRoute={{name: `Scene1`, index: 1,}}
         navigationBar={<Navbar/>}
         renderScene={this._renderScene}
-        configureScene={this._configureScene}
       />
     );
   }
 }
+
+export default App;
