eaafebf6d91690c7599d96eb985dc0a7383ce262
Max Petruck <petruck.maxim@gmail.com>
2016-01-19 20:27:28 +0200

    Update scene management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component, Navigator, } from 'react-native';
 import Navbar from './components/Navbar.js';
-import Scene from './components/Scene';
+import ButtonScene from './components/Scenes/ButtonScene.js';
+import ImageScene from './components/Scenes/ImageScene.js';
+import { BUTTON_SCENE, IMAGE_SCENE, } from './components/Scenes/SceneIndexes.js';
 
 const BaseConfig = Navigator.SceneConfigs.FloatFromBottom;
 const CustomGesture = Object.assign({}, BaseConfig.gestures.pop, {
   snapVelocity: 6,
 });
+
 const CustomSceneConfig = Object.assign({}, BaseConfig, {
   springTension: 100,
   springFriction: 6,
@@ -16,23 +19,13 @@ const CustomSceneConfig = Object.assign({}, BaseConfig, {
 
 class App extends Component {
   _renderScene(route, navigator) {
-    return (
-      <Scene
-        name={route.name}
-        onBack={() => {
-          if (route.index > 0) {
-            navigator.pop();
-          }
-        }}
-        onForward={() => {
-            let nextIndex = route.index + 1;
-            navigator.push({
-              name: `Scene` + nextIndex,
-              index: nextIndex,
-            });
-          }}
-      />
-    );
+    switch(route.index) {
+      case IMAGE_SCENE:
+      return <ImageScene/>;
+      case BUTTON_SCENE:
+      default:
+      return <ButtonScene/>;
+    }
   }
   _configureScene() {
     return CustomSceneConfig;
@@ -41,7 +34,7 @@ class App extends Component {
     return (
       <Navigator
         configureScene={this._configureScene}
-        initialRoute={{name: `Scene1`, index: 1,}}
+        initialRoute={{index: 1,}}
         navigationBar={<Navbar/>}
         renderScene={this._renderScene}
       />
