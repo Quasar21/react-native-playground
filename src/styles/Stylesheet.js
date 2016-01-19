cc2fc89eca07f23ce619491e35599ce1381f7463
Max Petruck <petruck.maxim@gmail.com>
2016-01-19 18:04:12 +0200

    Refactor stylesheet

--- src/styles/Stylesheet.js
+++ src/styles/Stylesheet.js
@@ -1,34 +1,34 @@
-import React, { StyleSheet } from 'react-native';
+import React, { StyleSheet, } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000000'
+    justifyContent: `center`,
+    alignItems: `center`,
+    backgroundColor: `#000000`,
   },
   navbarContainer: {
     flex: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flexWrap: 'nowrap'
+    flexDirection: `row`,
+    justifyContent: `space-around`,
+    flexWrap: `nowrap`,
   },
   button: {
-    backgroundColor: '#4E6E5E',
+    backgroundColor: `#4E6E5E`,
     borderRadius: 4,
     margin: 10,
-    padding: 5
+    padding: 5,
   },
   buttonText: {
     fontSize: 20,
-    textAlign: 'center',
-    color: '#FFFFFF'
+    textAlign: `center`,
+    color: `#FFFFFF`,
   },
   title: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: `center`,
+    color: `#333333`,
     fontSize: 30,
-    margin: 10
+    margin: 10,
   },
 });
 
