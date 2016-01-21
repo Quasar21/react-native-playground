9c99568e8c1b3bcc44bd52038155ced03fbd46a7
Max Petruck <petruck.maxim@gmail.com>
2016-01-21 15:39:51 +0200

    Add button rendering methods and few buttons for tests

--- src/components/Scenes/ButtonScene.js
+++ src/components/Scenes/ButtonScene.js
@@ -2,16 +2,51 @@ import React, {
   Component,
   Text,
   View,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import styles from '../../styles/Stylesheet.js';
 
 class ButtonScene extends Component {
+  _renderTouchableHighlight(text) {
+    return (
+      <TouchableHighlight style={styles.button}>
+        <Text style={styles.buttonText}>
+          {text}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+  _renderTouchableOpacity(text) {
+    return (
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+  _renderTouchableWithoutFeedback(text) {
+    return (
+      <TouchableWithoutFeedback style={styles.button}>
+        <Text style={styles.buttonText}>
+          {text}
+        </Text>
+      </TouchableWithoutFeedback>
+    );
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           Button Scene
         </Text>
+        <View>
+          {this._renderTouchableHighlight(`Highlight`)}
+          {this._renderTouchableOpacity(`Opacity`)}
+          {this._renderTouchableWithoutFeedback(`Without Feedback`)}
+        </View>
       </View>
     );
   }
