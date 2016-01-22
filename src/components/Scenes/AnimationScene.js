2bcd4f46352dd0592da3c2b00433cb29d3086d79
Max Petruck <petruck.maxim@gmail.com>
2016-01-22 17:29:23 +0200

    Add second component and parallel color animation for it

--- src/components/Scenes/AnimationScene.js
+++ src/components/Scenes/AnimationScene.js
@@ -12,36 +12,59 @@ const MyAnimatedText = Animated.createAnimatedComponent(Text);
 class AnimationScene extends Component {
   state = {
     fontSize: new Animated.Value(0),
+    color: new Animated.Value(0),
   };
   constructor(props: any) {
     super(props);
-    Animated.sequence([
-      Animated.spring(
-        this.state.fontSize,
-        {
-          toValue: 30,
-          friction: 1,
-          tension: 60,
-        }
-      ),
+    Animated.parallel([
+      Animated.sequence([
+        Animated.spring(
+          this.state.fontSize,
+          {
+            toValue: 30,
+            friction: 1,
+            tension: 60,
+          }
+        ),
+        Animated.timing(
+          this.state.fontSize,
+          {
+            toValue: 0,
+            duration: 5000,
+            delay: 0,
+            easing: Easing.inOut(Easing.linear),
+          }
+        ),
+      ]),
       Animated.timing(
-        this.state.fontSize,
+        this.state.color,
         {
-          toValue: 0,
+          toValue: 255,
           duration: 5000,
-          delay: 0,
-          easing: Easing.inOut(Easing.linear),
         }
       ),
     ]).start();
   }
   render() {
+    let color = this.state.color.interpolate({
+        inputRange: [0, 255,],
+        outputRange: [`rgba(255, 0, 0, 1)`, `rgba(0, 255, 0, 1)`,],
+    });
     return (
       <View style={styles.container}>
         <MyAnimatedText style={{
           textAlign: `center`,
           color: `#333333`,
           fontSize: this.state.fontSize,
+          margin: 10,
+        }}
+        >
+          Animation Scene
+        </MyAnimatedText>
+        <MyAnimatedText style={{
+          textAlign: `center`,
+          color: color,
+          fontSize: 40,
           margin: 10,
         }}
         >
